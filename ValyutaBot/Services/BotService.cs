ac3240d2d3671235dcc8170dbdd155c71981a782
xayotbeki <xayotbekdeveloper@gmial.com>
2024-11-20 11:31:57 +0500

    fullBot

---
+++ ValyutaBot/Services/BotService.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot;
+using System.Threading;
+using ValyutaBot.Handlers;
+using System.Threading.Tasks;
+
+namespace ValyutaBot.Services;
+
+public class BotService
+{
+    private readonly TelegramBotClient _token;
+    private readonly HandleUpdate _handleUpdate;
+    public BotService(string token, HandleUpdate handleUpdate)
+    {
+        _token = new TelegramBotClient(token);
+        _handleUpdate = handleUpdate;
+    }
+
+    public async Task StartReceiving(CancellationToken cancellationToken)
+    {
+        _token.StartReceiving(
+            _handleUpdate.HandleUpdateAsync,
+            HandleError.HandleErrorAsync,
+            cancellationToken: cancellationToken
+            );
+    }
+}
